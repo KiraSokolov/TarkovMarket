d61b31731cac46898f19b08c6521ddd8afc194ba
WillChew <williamchew95@gmail.com>
2020-03-08 16:53:37 -0400

    Initial Commit

---
+++ TarkovMarket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TarkovMarket
+//
+//  Created by Will Chew on 2020-03-08.
+//  Copyright © 2020 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
